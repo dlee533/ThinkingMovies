bb581c755c2bfd4bacedaaf044edcf6aa1aecec5
Andi Hung <chihninghung@gmail.com>
2021-11-04 18:59:38 -0700

    changed db to local db

--- src/server/app.js
+++ src/server/app.js
@@ -14,9 +14,9 @@ app.use(function(req, res, next) {
 //Create connection to db
 const db = mysql.createConnection({
     host: 'localhost',
-    user: 'admin',
-    password: 'lthinkingmovies',
-    database: 'andiclou_thinking_movies'
+    user: 'root',
+    password: '',
+    database: 'bucketlist',
 });
 
 db.connect((err) => {
@@ -24,38 +24,9 @@ db.connect((err) => {
     console.log("Connected!");
 });
 
-app.post(resource, (req, res) => {
-    let body = "";
-    req.on('data', function (chunk) {
-        if (chunk !== null) {
-            body += chunk;
-        }
-    });
-
-    req.on('end', () => {
-        let values = JSON.parse(body);
-        let sql = `INSERT INTO score(name, score) values ('${values.name}', ${values.score})`;
-        db.query(sql, (sqlErr, sqlRes) => {
-            if (sqlErr) {
-                res.status(404).send('There is some error here!');
-                throw err;
-            }
-            res.status(200).send(`${values.name}:${values.score} was stored in DB`);
-        });
-    });
-});
-
-app.get(resource, (req, res) => {
-    let sql = `SELECT * FROM score`;
-    db.query(sql, (err, result) => {
-        if (err) throw err;
-        res.status(200).send(`${JSON.stringify(result)}`);
-    });
-});
-
 app.listen(PORT, (err) => {
     if (err) throw err;
     console.log("Listening to port", PORT);
 });
 
-// app.listen();
+// // app.listen();

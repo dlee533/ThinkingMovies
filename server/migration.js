29b6e5f47ff2608da8c28d4aee5838de849c65d6
dongl <dmlee533@gmail.com>
2021-11-09 13:39:18 -0800

    edited db table definitions

--- server/migration.js
+++ server/migration.js
@@ -1,10 +1,21 @@
 const conn = require('./modules/db');
 const { createAdmin } = require('./models/user');
 const { encryptPassword } = require('./modules/password');
+//
+// user:
+// id, username, email, password
+//
+// bucketlist:
+// id, user_id, name
+//
+// bucketItems:
+// id, bucketlist_id, name
+
 
 const queryList = [
+  "DROP TABLE IF EXISTS bucketItem",
+  "DROP TABLE IF EXISTS bucketlist",
   "DROP TABLE IF EXISTS user",
-  
   "CREATE TABLE user (\
    id int(11) AUTO_INCREMENT,\
    username varchar(255),\
@@ -13,6 +24,18 @@ const queryList = [
    isAdmin boolean DEFAULT false,\
    PRIMARY KEY(id),\
    UNIQUE(email))",
+  "CREATE TABLE bucketlist (\
+   id int(11) AUTO_INCREMENT,\
+   name varchar(255),\
+   user_id int(11),\
+   PRIMARY KEY(id),\
+   FOREIGN KEY (user_id) REFERENCES user(id))",
+  "CREATE TABLE bucketItem (\
+   id int(11) AUTO_INCREMENT,\
+   name varchar(255),\
+   bucketlist_id int(11),\
+   PRIMARY KEY(id),\
+   FOREIGN KEY (bucketlist_id) REFERENCES bucketlist(id))",
 ]
 
 for (let i = 0; i < queryList.length; i++) {

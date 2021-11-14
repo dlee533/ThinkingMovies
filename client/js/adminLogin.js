337a894c16a80a200c59ea68124e6478353ac29d
dongl <dmlee533@gmail.com>
2021-11-13 19:29:17 -0800

    added adminLogin

--- client/js/adminLogin.js
+++ client/js/adminLogin.js
@@ -1,8 +1,8 @@
-const resource="/register";
+const xhttp = new XMLHttpRequest();
 const endPointRoot = 'http://localhost:8080';
+const resource="/adminLogin";
 
 const username = document.getElementById('username');
-const email = document.getElementById('email');
 const password = document.getElementById('password');
 const message = document.getElementById('message');
 
@@ -10,22 +10,19 @@ const loginHandler = (e) => {
   e.preventDefault();
 
   const xhttp = new XMLHttpRequest();
-
-  const paramsJson = {
-    'username': username.value,
-    'email': email.value,
-    'password': password.value
-  };
+  const paramsJson = {'username': username.value, 'password': password.value};
 
   xhttp.open("POST", endPointRoot+resource, true);
   xhttp.setRequestHeader('Content-type', 'application/json');
   xhttp.send(JSON.stringify(paramsJson));
   xhttp.onreadystatechange = () => {
       if (xhttp.readyState == 4 && xhttp.status == 200) {
-          alert("Successfully registered");
-          window.location.href = "./userLogin.html";
+          const res = JSON.parse(xhttp.responseText);
+          window.localStorage.setItem("adminToken", res.token);
+          alert(`${res.message}`);
+          // window.location.href = ""; // TODO: redirect to landing page
       } else if (xhttp.readyState == 4) {
-          message.textContent = `Error: ${JSON.parse(xhttp.responseText).message}`;
+          message.textContent  = `Error: ${JSON.parse(xhttp.responseText).message}`;
       }
   };
 }

ba89e66aaf55a3eaf645a51c84f459aa85a653d2
dongl <dmlee533@gmail.com>
2021-11-13 18:23:21 -0800

    added userlogin & register, integrated

---
+++ client/js/register.js
@@ -0,0 +1,34 @@
+const resource="/register";
+const endPointRoot = 'http://localhost:8080';
+
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const message = document.getElementById('message');
+
+const loginHandler = (e) => {
+  e.preventDefault();
+
+  const xhttp = new XMLHttpRequest();
+
+  const paramsJson = {
+    'username': username.value,
+    'email': email.value,
+    'password': password.value
+  };
+
+  xhttp.open("POST", endPointRoot+resource, true);
+  xhttp.setRequestHeader('Content-type', 'application/json');
+  xhttp.send(JSON.stringify(paramsJson));
+  xhttp.onreadystatechange = () => {
+      if (xhttp.readyState == 4 && xhttp.status == 200) {
+          alert("Successfully registered");
+          window.location.href = "./login.html";
+      } else if (xhttp.readyState == 4) {
+          message.textContent = `Error: ${JSON.parse(xhttp.responseText).message}`;
+      }
+  };
+}
+
+const form = document.getElementById('form');
+form.addEventListener('submit', loginHandler);
--- server/app.js
+++ server/app.js
@@ -8,11 +8,13 @@ const authController = require('./controllers/auth');
 const user = require('./models/user');
 const createToken = require('./modules/createToken');
 const decodeToken = require('./modules/decodeToken');
+const errorHandler = require('./modules/errorHandler');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
+    console.log(`Request: ${req.method}, ${req.path}`);
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
@@ -26,14 +28,7 @@ app.post('/adminLogin', authController.adminLogin);
 app.post('/userLogin', authController.userLogin);
 app.post('/register', authController.register);
 
-app.use((err, req, res, next) => {
-  console.log(err);
-  const statusCode = err.code && (err.code >= 100 && err.code < 600) ? err.code : 400;
-  res.status(statusCode)
-     .json({
-       message: err.message
-     });
-})
+app.use(errorHandler);
 
 app.listen(PORT, (err) => {
     if (err) throw err;
